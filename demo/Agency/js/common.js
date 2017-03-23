8334794ef7a84dea45269734a72c83c5cddb30bf
kaplansky <leon2796@mail.ru>
2017-03-23 17:19:30 +0300

    upp

---
+++ demo/Agency/js/common.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	$(".port_item").animated("flash", "shake")
+
+});
+
+
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	try {
+		$.browserSelectsor();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+});
+
